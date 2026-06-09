30925164a3d576e6c9f1518c77d969fa2deaa782
Ihor Kuzmychov <iikuzmychov@gmail.com>
2026-06-09 03:23:21 +0200

    feat(core): add FromNullable/TryFromNullable extensions

---
+++ src/SuperStrong.Types/StrongTypeClassExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace SuperStrong.Types;
+
+public static class StrongTypeClassExtensions
+{
+    extension<TStrongType, TPrimitive>(IStrongType<TStrongType, TPrimitive>)
+        where TStrongType : class, IStrongType<TStrongType, TPrimitive>
+        where TPrimitive : struct
+    {
+        public static TStrongType? FromNullable(TPrimitive? value)
+        {
+            if (value is null)
+            {
+                return null;
+            }
+
+            return TStrongType.From(value.Value);
+        }
+
+        public static bool TryFrom(TPrimitive? value, out TStrongType? result)
+        {
+            if (value is null)
+            {
+                result = null;
+                return true;
+            }
+
+            return TStrongType.TryFrom(value.Value, out result);
+        }
+    }
+
+    extension<TStrongType, TPrimitive>(IStrongType<TStrongType, TPrimitive>)
+        where TStrongType : class, IStrongType<TStrongType, TPrimitive>
+        where TPrimitive : class
+    {
+        public static TStrongType? FromNullable(TPrimitive? value)
+        {
+            if (value is null)
+            {
+                return null;
+            }
+
+            return TStrongType.From(value);
+        }
+
+        public static bool TryFrom(TPrimitive? value, out TStrongType? result)
+        {
+            if (value is null)
+            {
+                result = null;
+                return true;
+            }
+
+            return TStrongType.TryFrom(value, out result);
+        }
+    }
+}
