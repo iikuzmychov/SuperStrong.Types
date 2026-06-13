bad85c538068656b36919cca17a8fe35817ae640
Ihor Kuzmychov <iikuzmychov@gmail.com>
2026-06-13 02:11:19 +0200

    chore(systemtextjson): move SystemTextJson converter to the core project

--- src/SuperStrong.Types/Converters/JsonStrongTypeConverter.cs
+++ src/SuperStrong.Types/Converters/JsonStrongTypeConverter.cs
@@ -2,7 +2,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace SuperStrong.Types.SystemTextJson;
+namespace SuperStrong.Types.Converters;
 
 public sealed class JsonStrongTypeConverter : JsonConverterFactory
 {
